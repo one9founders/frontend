a1df22f74f1feb648d84eeab17ec79672de9ca24
One9Founders <one9founders@gmail.com>
2026-02-28 06:21:49 +0000

    fix: Remove internship button from navbar (desktop + mobile)

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -146,11 +146,6 @@ export default function Navbar() {
               </button>
             </div>
             <div className="flex items-center gap-4">
-              <Link href="/internship">
-                <button className="btn-primary px-4 py-2 hover:scale-105 transition-transform cursor-pointer">
-                  Internship
-                </button>
-              </Link>
               {/* <button className="btn-primary px-4 py-2" onClick={handleSubmitTool}>
                 Submit Tool
               </button> */}
@@ -235,11 +230,6 @@ export default function Navbar() {
                 Search Tools
               </button>
               <div className="flex flex-col gap-3 pt-2 border-t border-[var(--gray-800)]">
-                <Link href="/internship" onClick={() => setIsMobileMenuOpen(false)}>
-                  <button className="btn-primary px-4 py-2 text-left w-full cursor-pointer">
-                    Internship
-                  </button>
-                </Link>
                 {/* <button className="btn-primary px-4 py-2 text-left" onClick={handleSubmitTool}>
                   Submit Tool
                 </button> */}
